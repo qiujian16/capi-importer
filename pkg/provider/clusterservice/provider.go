5cf24b2b36e051bd9a4aad765f18af8b4894fa07
Wei Liu <liuweixa@redhat.com>
2024-01-09 20:32:35 +0800

    test ocm api
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- pkg/provider/clusterservice/provider.go
+++ pkg/provider/clusterservice/provider.go
@@ -3,18 +3,24 @@ package clusterservice
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"time"
 
 	sdk "github.com/openshift-online/ocm-sdk-go"
 	clustersmgmtv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 	"github.com/openshift-online/rh-trex/pkg/errors"
+	"github.com/openshift/library-go/pkg/oauth/tokenrequest"
 	"github.com/qiujian16/capi-importer/pkg/provider"
+	"github.com/sethvargo/go-password/password"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	clientcmdlatest "k8s.io/client-go/tools/clientcmd/api/latest"
 	clusterapiv1 "open-cluster-management.io/api/cluster/v1"
 )
 
@@ -24,6 +30,8 @@ type ClusterServiceProvider struct {
 	handler cache.ResourceEventHandler
 	store   cache.Store
 	token   string
+
+	clusterAdmins map[string]*clustersmgmtv1.HTPasswdIdentityProvider
 }
 
 func NewClusterServiceProvider(token string) provider.ClusterProvider {
@@ -31,7 +39,8 @@ func NewClusterServiceProvider(token string) provider.ClusterProvider {
 		store: cache.NewIndexer(clusterKey, cache.Indexers{
 			byKey: cache.MetaNamespaceIndexFunc,
 		}),
-		token: token,
+		token:         token,
+		clusterAdmins: make(map[string]*clustersmgmtv1.HTPasswdIdentityProvider),
 	}
 }
 
@@ -92,7 +101,9 @@ func (c *ClusterServiceProvider) poll() {
 
 	// Get the client for the resource that manages the collection of clusters:
 	collection := connection.ClustersMgmt().V1().Clusters()
-	clusterList, err := collection.List().Send()
+
+	// TODO need more search params like cluster status, archived cluster, etc
+	clusterList, err := collection.List().Search("product.id='rosa'").Send()
 	if err != nil {
 		logger.Error(context.TODO(), err.Error())
 		return
@@ -108,12 +119,15 @@ func (c *ClusterServiceProvider) poll() {
 				},
 			},
 		}
-		clusterCredential, err := collection.Cluster(cluster.ID()).Credentials().Get().Send()
+
+		kubeconfig, err := c.getClusterKubeconfig(collection.Cluster(cluster.ID()), cluster)
 		if err != nil {
 			logger.Error(context.TODO(), err.Error())
 			return true
 		}
-		mcl.Annotations["kubeconfig"] = clusterCredential.Body().Kubeconfig()
+
+		mcl.Annotations["kubeconfig"] = kubeconfig
+
 		if _, exists, _ := c.store.GetByKey(mcl.Name); exists {
 			return true
 		}
@@ -129,4 +143,113 @@ func (c *ClusterServiceProvider) poll() {
 func clusterKey(obj interface{}) (string, error) {
 	accesor, err := meta.Accessor(obj)
 	return accesor.GetName(), err
+}
+
+func (c *ClusterServiceProvider) getClusterKubeconfig(clusterClient *clustersmgmtv1.ClusterClient, cluster *clustersmgmtv1.Cluster) (string, error) {
+	credentialsResp, err := clusterClient.Credentials().Get().Send()
+	if err != nil {
+		if credentialsResp != nil && credentialsResp.Status() == http.StatusForbidden {
+			api, ok := cluster.GetAPI()
+			if !ok {
+				return "", fmt.Errorf("failed to get cluster api for cluster %q", cluster.ID())
+			}
+
+			// TODO need to save the cluster admin to a secret instead of in a cache
+			clusterAdmin, ok := c.clusterAdmins[cluster.ID()]
+			if !ok {
+				clusterAdmin, err = createClusterAdmin(clusterClient)
+				if err != nil {
+					return "", err
+				}
+
+				c.clusterAdmins[cluster.ID()] = clusterAdmin
+			}
+
+			kubeconfig, err := buildKubeConfig(api.URL(), clusterAdmin)
+			if err != nil {
+				return "", err
+			}
+
+			return string(kubeconfig), nil
+		}
+
+		return "", err
+	}
+
+	return credentialsResp.Body().Kubeconfig(), nil
+}
+
+func createClusterAdmin(clusterClient *clustersmgmtv1.ClusterClient) (*clustersmgmtv1.HTPasswdIdentityProvider, error) {
+	user, err := clustersmgmtv1.NewUser().ID("acm-bootstrap-admin").Build()
+	if err != nil {
+		return nil, err
+	}
+
+	pw, err := password.Generate(20, 10, 0, false, false)
+	if err != nil {
+		return nil, err
+	}
+
+	htPasswdUserBuilder := clustersmgmtv1.NewHTPasswdUser().Username("acm-bootstrap-admin").Password(pw)
+	htPasswdUserListBuilder := clustersmgmtv1.NewHTPasswdUserList().Items(htPasswdUserBuilder)
+	htPasswdUsers := clustersmgmtv1.NewHTPasswdIdentityProvider().Users(htPasswdUserListBuilder)
+	htPasswdIDProvider, err := htPasswdUsers.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	idProvider, err := clustersmgmtv1.NewIdentityProvider().
+		Name("acm-bootstrap-admin").
+		Type(clustersmgmtv1.IdentityProviderTypeHtpasswd).
+		Htpasswd(htPasswdUsers).
+		Build()
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := clusterClient.Groups().Group("cluster-admins").Users().Add().Body(user).Send(); err != nil {
+		return nil, err
+	}
+
+	if _, err := clusterClient.IdentityProviders().Add().Body(idProvider).Send(); err != nil {
+		return nil, err
+	}
+
+	return htPasswdIDProvider, nil
+}
+
+func buildKubeConfig(apiURL string, admin *clustersmgmtv1.HTPasswdIdentityProvider) ([]byte, error) {
+	var username, password string
+	admin.Users().Each(func(user *clustersmgmtv1.HTPasswdUser) bool {
+		username = user.Username()
+		password = user.Password()
+		return true
+	})
+
+	token, err := tokenrequest.RequestTokenWithChallengeHandlers(&rest.Config{
+		Host:     apiURL,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	bootstrapKubeconfig := &clientcmdapi.Config{
+		Clusters: map[string]*clientcmdapi.Cluster{"default-cluster": {
+			Server:                apiURL,
+			InsecureSkipTLSVerify: true,
+		}},
+		AuthInfos: map[string]*clientcmdapi.AuthInfo{"default-auth": {
+			Token: string(token),
+		}},
+		Contexts: map[string]*clientcmdapi.Context{"default-context": {
+			Cluster:   "default-cluster",
+			AuthInfo:  "default-auth",
+			Namespace: "default",
+		}},
+		CurrentContext: "default-context",
+	}
+
+	return runtime.Encode(clientcmdlatest.Codec, bootstrapKubeconfig)
 }
